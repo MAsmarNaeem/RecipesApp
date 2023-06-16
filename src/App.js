57044a799cb6451c9918b7e20cc68200a6a8b5fd
-e <asmarnaeem35@gmail.com>
2023-06-16 15:08:13 +0500

    push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import Recipiee from "./Recipie";
+import { queries } from "@testing-library/react";
 
 function App() {
+  const [search, setsearch] = useState("");
+  const [recipies, setrecipies] = useState([]);
+  const [query, setquery] = useState("chicken");
+  const App_ID = "60bc3f3a";
+  const App_Key = "e117d1f52556736684ee7e9dee1ecfdd";
+  useEffect(() => {
+    getRecipes();
+  }, [query]);
+
+  const getRecipes = async () => {
+    try {
+      const response = await fetch(
+        `https://api.edamam.com/search?q=${query}&app_id=f8ce441a&app_key=0acaacd8401f62e14dcbd30385b794e1`
+      );
+      if (!response.ok) {
+        throw new Error("Request failed with status " + response.status);
+      }
+      const data = await response.json();
+      // console.log("data is:", data.hits);
+      setrecipies(data.hits);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const updateSearch = (e) => {
+    setsearch(e.target.value);
+    console.log("sreach is ", search);
+  };
+  const getSearch = (e) => {
+    e.preventDefault();
+    setquery(search);
+    console.log("wuery is ", query);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={getSearch} className="search-form">
+        <input
+          className="search-bar"
+          placeholder="Search recipes"
+          type="text"
+          value={search}
+          onChange={updateSearch}
+        />
+        <button className="search-button">search</button>
+      </form>
+<div className="recipes">
+      {recipies.map((recipi) => {
+        return (
+          <Recipiee
+            key={recipi.recipe.label}
+            title={recipi.recipe.label}
+            calories={recipi.recipe.calories}
+            image={recipi.recipe.image}
+            ingredients={recipi.recipe.ingredients}
+          />
+        );
+      })}</div>
     </div>
   );
 }
